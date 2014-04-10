0c1b787c0db1357ca730e4708594fed4c303cc55
Paul Anderson <dcspaul@ed.ac.uk>
2014-04-10 14:24:46 +0100

    added support for comment. tidied parser

--- hsfparser.hs
+++ hsfparser.hs
@@ -21,7 +21,10 @@ data Prototype = RefProto Reference | BodyProto Body deriving(Show)
 
 lexer = P.makeTokenParser emptyDef {
 	reservedNames = ["true", "false","NULL","DATA","extends"],
-	reservedOpNames = ["{","}","[","]"]
+	reservedOpNames = ["{","}","[","]"],
+	commentStart = "/*",
+	commentEnd = "*/",
+	commentLine = "//"
 	}
 
 m_identifier = P.identifier lexer
@@ -34,15 +37,17 @@ m_colon = P.colon lexer
 m_semi = P.semi lexer
 m_comma = P.comma lexer
 m_commaSep = P.commaSep lexer
+m_braces = P.braces lexer
+m_brackets = P.brackets lexer
 
 -- **** the parser
 
 ident :: Parser Identifier
-ident = do { ident <- m_identifier ; return (Identifier ident) }
+ident = do { i <- m_identifier ; return (Identifier i) }
 
 -- R :: = I(:I)*
 reference :: Parser Reference
-reference = do { id_list <- ident `sepBy1` m_colon ; return (Reference id_list) }
+reference = do { ref <- ident `sepBy1` m_colon ; return (Reference ref) }
 
 -- LR ::= R
 linkref :: Parser LinkRef
@@ -50,19 +55,21 @@ linkref = do { (Reference ref) <- reference ; return (LinkRef ref) }
 
 -- BV ::= Bool | Num | Str | DATA R | Null | Vector [BV]
 basicValue :: Parser BasicValue
-basicValue = do { num <- m_stringLiteral ; return (StringValue num) }
-	<|> do { num <- m_integer ; return (NumValue num) }
+basicValue = do { s <- m_stringLiteral ; return (StringValue s) }
+	<|> do { n <- m_integer ; return (NumValue n) }
 	<|> do { m_reserved "true" ; return (BoolValue True) }
 	<|> do { m_reserved "false" ; return (BoolValue False) }
 	<|> do { m_reserved "NULL" ; return NullValue }
 	<|> do { m_reserved "DATA" ; (Reference ref) <- reference ; return (DataRef ref) }
-	<|> do { m_reservedOp "[" ; v <- m_commaSep basicValue; m_reservedOp "]" ; return (Vector v) }
+	<|> do { v <- m_brackets bvList ; return (Vector v) }
+	where bvList = do { bvs <- m_commaSep basicValue; return (bvs) }
 
 -- V ::= BV ; | LR ; | extends [PS]
 value :: Parser Value
 value = do { bv <- basicValue ; m_semi ; return (BasicValue bv) }
 	<|> do { lr <- linkref ; m_semi ; return (LinkValue lr) }
-	<|> do { m_reserved "extends"; elt_list <- prototype `sepBy1` m_comma ; return (ProtoValue elt_list) }
+	<|> do { m_reserved "extends"; ps <- protoList; return (ProtoValue ps) }
+	where protoList = do { ps <- prototype `sepBy1` m_comma; return (ps) }
 
 -- A ::= R V
 assignment :: Parser Assignment
@@ -70,20 +77,20 @@ assignment = do { lhs <- reference ; rhs <- value ; return (Assignment lhs rhs) 
 
 -- B ::= [A]
 body :: Parser Body
-body = do { assignment_list <- assignment `sepBy` m_whiteSpace ; return (Body assignment_list) }
+body = do { as <- assignment `sepBy` m_whiteSpace ; return (Body as) }
 
 -- P ::= R | { B }
 prototype :: Parser Prototype
 prototype = do { ref <- reference ; return (RefProto ref) }
-	<|> do { try (m_reservedOp "{") ; b <- body ; try (m_reservedOp "}") ; return (BodyProto b) }
+	<|> do { b <- m_braces body ; return (BodyProto b) }
 
 -- SF ::= B <eof>
 specification :: Parser Body
-specification = do { b <- body ; eof ; return b }
+specification = do { m_whiteSpace; b <- body ; eof ; return b }
 
 -- *** main
 
-main = do { result <- parseFromFile specification "/Users/paul/Work/Playground/HaskellSF/Test/herry4.sf"
+main = do { result <- parseFromFile specification "/Users/paul/Work/Playground/HaskellSF/Test/features.sf"
 	; case (result) of
 		Left err  -> print err
 		Right strings  -> print strings
