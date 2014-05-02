0b8cd710bc6a5596359b19308b88c35bb08b69d8
Paul Anderson <dcspaul@ed.ac.uk>
2014-05-02 16:52:42 +0100

    fixed formatting of parse error messages

--- hsf.hs
+++ hsf.hs
@@ -432,7 +432,9 @@ compile isComparing sourcePath destPath = do
 		Left e -> if (isComparing)
 			then writeFile destPath e
 			else hPutStrLn stderr ( "** " ++ sourcePath ++ "\n" ++ e )
-		Right json -> writeFile destPath json
+		Right json -> if (destPath == "-")
+			then putStr json
+			else writeFile destPath json
 	-- return the results or the error message
 	case (result) of
 		Left e -> return e
@@ -451,8 +453,8 @@ data ErrorCode = EPARSEFAIL | EPARENTNOTSTORE | ENOPARENT | EREPLACEROOTSTORE |
 err :: ErrorCode -> [String] -> Bool -> String
 err code args = \isComparing -> case (code) of
 	EPARSEFAIL	-> if (isComparing)
-		then (show a) ++ "\n"
-		else "parse failed: " ++ (show a) ++ "\n"
+		then (id a) ++ "\n"
+		else "parse failed: " ++ (id a) ++ "\n"
 	EPARENTNOTSTORE -> "parent not a store [error 1]: " ++ a
 	ENOPARENT -> "reference has no parent [error 2]: " ++ a
 	EREPLACEROOTSTORE -> "attempt to replace root store [error 3]"
@@ -480,6 +482,7 @@ err code args = \isComparing -> case (code) of
 -- you can use ".." in this to refer to directories above the source
 -- with a slash it is treated as an absolute pathname
 -- the default is "" which is the same directory as the source
+-- you can use "-" for stdout
 
 data Flag = Output String | Compare | SfParser String deriving(Show,Eq)
 
@@ -534,7 +537,9 @@ main = do
 
 process :: [Flag] -> String -> IO ()
 process opts srcPath = do
-	let dstPath = jsonPath srcPath (outputDir opts)
+	let dstPath = if ((outputDir opts) == "-")
+		then (++) "-"
+		else jsonPath srcPath (outputDir opts)
 	if (Compare `elem` opts) then do
 		sfParserPath <- (findSfParserPath opts)
 		haskellResult <- compile True srcPath (dstPath "-1")
